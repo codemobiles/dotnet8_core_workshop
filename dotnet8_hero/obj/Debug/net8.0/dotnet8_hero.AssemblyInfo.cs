0ce35817c8ead47739cfb8956342aa43921f1cb0
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-06-26 12:33:52 +0700

    dotnet8_hero_ep3

--- dotnet8_hero/obj/Debug/net8.0/dotnet8_hero.AssemblyInfo.cs
+++ dotnet8_hero/obj/Debug/net8.0/dotnet8_hero.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet8_hero")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b27bf9352f68df2d792645ec4a565e8aeeff24a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+154d227bdd32595873d8dd9eac0bc1ccb06d32d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet8_hero")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet8_hero")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
