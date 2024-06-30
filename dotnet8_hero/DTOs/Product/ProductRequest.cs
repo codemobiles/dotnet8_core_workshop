5755ba0b9cb9b94cf69b2bb5a25af33075e5f86e
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-06-30 11:33:30 +0700

    dotnet8_hero_ep19

--- dotnet8_hero/DTOs/Product/ProductRequest.cs
+++ dotnet8_hero/DTOs/Product/ProductRequest.cs
@@ -1,10 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace dotnet8_hero.DTOs.Product
 {
     public class ProductRequest
     {
+        [Required]
+        [MaxLength(100, ErrorMessage = "Name, maximum length 100")]
         public string Name { get; set; } = null!;
+
+        [Range(0, 10000, ErrorMessage = "Stock, Range must be between 0 and 10000")]
         public int Stock { get; set; }
+
+        [Range(0, 10000)]
         public decimal Price { get; set; }
+
         public int CategoryId { get; set; }
         public List<IFormFile>? FormFiles { get; set; }
     }
